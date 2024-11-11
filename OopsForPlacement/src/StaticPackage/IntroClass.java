53f7a94faf0d0e18d33472cc5ac956e715d9a7ed
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2024-11-11 18:53:31 +0530

    importing in java

---
+++ OopsForPlacement/src/StaticPackage/IntroClass.java
@@ -0,0 +1,8 @@
+package StaticPackage;
+import static Introduction.Greeting.greetUser;
+public class IntroClass {
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+       greetUser();
+    }
+}
