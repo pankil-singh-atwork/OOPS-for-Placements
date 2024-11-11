d3a7e2208853f730c13eabcedfe9399e7ee848df
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2024-11-11 18:50:56 +0530

    greeting file

---
+++ OopsForPlacement/src/Introduction/Greeting.java
@@ -0,0 +1,11 @@
+package Introduction;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    public static void greetUser() {
+        System.out.println("Hello User!! This method is imported from external package");
+    }
+    
+}
