4b1987ce60a1d8e412b7102ff484486a0917beb4
pankil-singh-atwork <pankilkumar.singh2020@vitstudent.ac.in>
2024-11-12 23:17:58 +0530

    singleton intro

---
+++ OopsForPlacement/src/SingletonPackage/SingletonClass.java
@@ -0,0 +1,10 @@
+package SingletonPackage;
+
+class Singleton {
+    private Singleton() {};
+}
+public class SingletonClass {
+    public static void main(String[] args) {
+        // Singleton obj1 = new SingletonClass();
+    }
+}
